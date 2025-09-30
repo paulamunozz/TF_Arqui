6c29e5aecd9fe55d49141bc331e33c8e926bbfbd
paulamunozz <u202312321@upc.edu.pe>
2025-09-30 16:48:48 -0500

    ComentariosEventoDTO y logs

---
+++ Ecocycle/src/main/java/com/upc/ecocycle/dto/ComentariosEventoDTO.java
@@ -0,0 +1,15 @@
+package com.upc.ecocycle.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ComentariosEventoDTO {
+    private String nombre;
+    private String comentario;
+}
