8e65792104ca142bbb5c2b19a71918cde49d5c3b
paulamunozz <u202312321@upc.edu.pe>
2025-10-27 10:02:48 -0500

    Carpeta para frontend

--- Ecocycle/src/main/java/com/upc/ecocycle/security/repositories/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.upc.ecocycle.security.repositories;
-
-import com.upc.ecocycle.security.entities.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-}
--- Ecocycle/src/main/java/com/upc/ecocycle/validations/Create.java
+++
@@ -1,4 +0,0 @@
-package com.upc.ecocycle.validations;
-
-public interface Create {
-}
