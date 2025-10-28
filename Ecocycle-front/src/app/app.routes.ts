6d5c99281b67b2888090161d4fff8b7f55701551
paulamunozz <u202312321@upc.edu.pe>
2025-10-27 20:49:51 -0500

    US21 y US22

--- Ecocycle-front/src/app/app.routes.ts
+++ Ecocycle-front/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import {MunicipalidadEventos} from './us19-us20-municipalidad-eventos/municipalidad-eventos';
 import {MunicipalidadRegistroEvento} from './us16-municipalidad-registro-evento/municipalidad-registro-evento';
+import {MunicipalidadDetalleEvento} from './us21-us22-us23-municipalidad-detalle-evento/municipalidad-detalle-evento';
 
 export const routes: Routes = [
   {path:'', component:MunicipalidadEventos},
   {path:'eventos', component:MunicipalidadEventos},
   {path:'nuevo-evento', component:MunicipalidadRegistroEvento},
+  {path:'evento/:id', component:MunicipalidadDetalleEvento},
   {path:'**', redirectTo:''}
 ];
