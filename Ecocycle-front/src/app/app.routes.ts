6f34baa34d832fabf851b7280018d74afb6ea89a
carlosgouroc <carlosgcepeda@gmail.com>
2025-11-04 00:59:31 -0500

    HU01, HU03, HU04 en HTML

--- Ecocycle-front/src/app/app.routes.ts
+++ Ecocycle-front/src/app/app.routes.ts
@@ -7,7 +7,7 @@ import {
   MunicipalidadDetalleEvento
 } from './components/us21-us22-us23-municipalidad-detalle-evento/municipalidad-detalle-evento';
 import {VecinoRegistro} from './components/us05-vecino-registro/vecino-registro';
-import {VecinoAutenticacion} from './components/vecino-autenticacion/vecino-autenticacion';
+import {VecinoAutenticacion} from './components/us01-vecino-autenticacion/vecino-autenticacion';
 import {VecinoEventoConfirmacion} from './components/us25-vecino-evento-confirmacion/vecino-evento-confirmacion';
 import {
   MunicipalidadEventoConfirmacion
@@ -24,6 +24,11 @@ import {VecinoEventosRegistrados} from './components/us27-vecino-eventos-registr
 import {
   VecinoDetalleEventoRegistrado
 } from './components/us21-us26-us31-vecino-detalle-evento-registrado/vecino-detalle-evento-registrado';
+import {PerfilModificarEliminarComponent} from './components/us06-perfil-modificar-eliminar/perfil-modificar-eliminar';
+import {InicioComponent} from './components/us04-inicio-vecino/inicio-vecino';
+import {InicioMunicipalidad} from './components/us04-inicio-municipalidad/inicio-municipalidad';
+import {Contactanos} from './components/us04-contactanos/contactanos';
+import {Consejos} from './components/us03-consejos/consejos';
 
 export const routes: Routes = [
   { path: '', component: VecinoAutenticacion },
@@ -41,5 +46,10 @@ export const routes: Routes = [
   { path: 'detalle-evento-disponible/:id', component: VecinoDetalleEventoDisponible },
   { path: 'mis-eventos', component: VecinoEventosRegistrados },
   { path: 'detalle-evento-registrado/:id', component: VecinoDetalleEventoRegistrado },
+  { path: 'perfil', component: PerfilModificarEliminarComponent},
+  { path: 'inicio-vecino', component: InicioComponent},
+  { path: 'inicio-muni', component: InicioMunicipalidad},
+  { path: 'contacto', component: Contactanos},
+  { path: 'consejos', component: Consejos},
   { path: '**', redirectTo: '' }
 ];
